c715fa57d1701a82835036a82e3dbb344c689e25
rgoshen <rick.goshen@gmail.com>
2021-11-12 11:26:04 -0700

    added time and date to merge output file

--- pdf-merge.py
+++ pdf-merge.py
@@ -1,5 +1,8 @@
 import PyPDF2
 import sys
+import time
+
+timestr = time.strftime("%Y%m%d-%H%M%S")
 
 input_files = sys.argv[1:]
 
@@ -8,7 +11,7 @@ def pdf_merge(pdf_list):
     merger = PyPDF2.PdfFileMerger()
     for pdf in pdf_list:
         merger.append(pdf)
-    merger.write('merged.pdf')
+    merger.write(f'merged_{timestr}.pdf')
     print('all files merged')
 
 
